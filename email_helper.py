beecbfb53bdcc76893ef558de3cca0acb1e5fb37
Justus <justus@justcoding.tech>
2025-02-23 13:29:19 +0100

    Add recipient address to email header

--- email_helper.py
+++ email_helper.py
@@ -1,7 +1,7 @@
 import os
-from email.policy import SMTPUTF8
-from mailbox import Message
-from smtplib import SMTP, SMTP_SSL
+from email.message import EmailMessage
+from smtplib import SMTP
+
 from dotenv import load_dotenv
 
 
@@ -17,12 +17,18 @@ def init():
 def test(mail_address: str):
     smtp.sendmail(os.environ.get("SMTP_USER"), mail_address, "Subject: Test\nThis is a test email. äüö", mail_options=["smtputf8"])
 
-def send_mail(receiver_address: str, subject: str, body: str, sender_address: str = "admin@vcp-medingen.de"):
-    message = Message()
-    message.set_payload(body, charset="utf-8")
+def send_mail(receiver_address: str, subject: str, body: str, sender_address: str = "admin@vcp-medingen.de", html: bool = False):
+    message = EmailMessage()
+    if html:
+        message.set_content(body, subtype="html", charset="utf-8")
+    else:
+        message.set_payload(body, charset="utf-8")
     message["Subject"] = subject
+    message["From"] = sender_address
+    message["To"] = receiver_address
 
     smtp.send_message(message, sender_address, receiver_address, mail_options=["SMTPUTF8"])
+
 
 def close():
     smtp.quit()
